8bc0b08da82ece9ea12afec6da22733aee28d07d
tharindu97 <tharindukavishna@gmail.com>
2019-12-18 17:51:42 +0530

    3_3

---
+++ components/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div> Hi Pakaya</div>
+}
+
+ReactDOM.render(<App /> , document.querySelector('#root'));
