ff0c1d5324a3278e61cec1613d27c7474eda62d1
tharindu97 <tharindukavishna@gmail.com>
2019-12-25 20:31:37 +0530

    6-32

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,31 +8,22 @@ class App extends React.Component {
         // this is the only time we do direct
         this.state = { lat: null, errorMessage: ''};
 
+    }
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // We called setState
-                this.setState({lat: position.coords.latitude });
-                
-                // We did not!!!
-                //this.state.lat = position.coords.latitude
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })  
         );
     }
-    componentDidMount(){
-        console.log('my componet was rendered to the screen');
-    }
-    componentDidUpdate(){
-        console.log('my component was just update - it render');
-    }
+    
+    
         render() {
             if(this.state.errorMessage && !this.state.lat){
             return <div> Error: {this.state.errorMessage}</div>
             }
 
-            if(!this.state.errorMessage && this.state.errorMessage){
+            if(!this.state.errorMessage && this.state.lat){
             return <div>Latitude: {this.state.lat}</div>
             }
 
