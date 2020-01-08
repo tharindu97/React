381d0ab4c4932f672e5cf5c45aa6e55739493ac9
tharindu97 <tharindukavishna@gmail.com>
2020-01-08 14:29:29 +0530

    7_4

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import SearchBar from './SearchBar';
 
 const App = () => {
-    return <div>Tharindu</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    ); 
 };
 
 export default App;
