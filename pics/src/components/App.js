1abc05ae81ae608c75786b3d48d53ff52849b20d
tharindu97 <tharindukavishna@gmail.com>
2020-01-20 12:30:21 +0530

    8-3

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,7 +5,13 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
 
     onSearchSubmit(term){
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: { query: term },
+            headers: {
+                Authorization: 
+                'Client-ID 2fbd2b17907d4fa4fc55cdaa3213b5c092a674d30ca5affa1e988f6df9dcdf6a'
+            }
+        });
     }
     render() {
         return (
