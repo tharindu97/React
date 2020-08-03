a4648e323c17091b449120af23bcd4880b3e07d2
tharindu97 <tharindukavishna@gmail.com>
2020-08-03 21:49:40 +0530

    11_6

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -11,9 +11,9 @@ class SearchBar extends React.Component{
   onFormSubmit = (event) => {
     event.preventDefault();
     // TODO: Make sure we call
-    // callback from parent compponent
+    
   };
-
+ 
   render(){
     return (
       <div className="search-bar ui segment">
