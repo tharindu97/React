6d440d4540f6555f4aea702ac814a5f6dded4594
tharindu97 <tharindukavishna@gmail.com>
2020-08-21 19:00:19 +0530

    11_7

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBUvpe3Wh1zw2Qtj0u-9Xqk4RrezxfxKzI';
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    params:{
+        part:'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
